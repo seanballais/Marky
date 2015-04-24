567805d1a6537d7458c4e971e3fd1fb26f047dd7
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-04-24 21:52:47 +0800

    Added the necessary element blocks in the website.

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+if (typeof jQuery === 'undefined') { throw new Error('Marky requires jQuery') }
+
+// HTML element appearance modifications
+/* Add line numbers to the text area */
+
+// User interactions with Marky
