8a89646f580eb3d444f33a87496e36f3dee4c094
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-04-26 22:24:49 +0800

    Added the intro text

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,12 +40,19 @@ function resetThenColor(element) {
     $(element).css("cursor", "initial");
 }
 
+function initPage() {
+    resetThenColor("#intro-link");
+    $("#topic-title").html("Introduction<small>What is Markdown?</small>");
+    $("span#info-text > ul li").css("display", "none");
+    $("p#topic-instructions").html("According to Wikipedia, Markdown is a markup language with plain text formatting syntax designed so that it can be converted to HTML and many other formats using a tool by the same name. Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.<br><br>To start learning about markdown, press the 'Next' button on the lower right.");
+    $("div#markdown-playground").css("display", "none");
+    $("button#submit-md").prop("class", "btn btn-primary disabled");
+}
+
 // User interactions with Marky
 $(document).ready(function() {
-    // Initial effects
+    initPage(); // Initial set-up
     
-
-
     // Make the selected <li> 'active'
     $("li").click(function() {
         var li_ID = $(this).attr("id");
