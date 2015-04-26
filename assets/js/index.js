92b7308b62ac4c98adaa9057d894f66ac246eeba
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-04-26 21:19:57 +0800

    Added a nice effect when selecting an item from the menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,52 @@
 if (typeof jQuery === "undefined") { throw new Error("Marky requires jQuery") }
 
+// Global variables
+var links = [
+    [
+        "intro-link",
+        "basics-link",
+        "headers-link",
+        "links-link",
+        "codes-link",
+        "lists-link",
+        "quotes-link",
+        "images-link",
+        "paragraphs-link",
+        "play-link"
+    ],
+    [
+        "subtopic-1",
+        "subtopic-2",
+        "subtopic-3",
+        "subtopic-4",
+        "subtopic-5",
+        "subtopic-6"
+    ]
+];
+
+// Functions
+function resetThenColor(element) {
+    var listIndex = 0;
+
+    // Check which nav list is the selected element in
+    if (element.indexOf("subtopic") > -1) { listIndex = 1; } // Selected element is in the subtopic nav list
+
+    for (var counter = 0; counter < links[listIndex].length; counter++) {
+        $("#" + links[listIndex][counter]).css("border-bottom-color", "#ffffff");
+        $("#" + links[listIndex][counter]).css("cursor", "pointer");
+    }
+
+    $(element).css("border-bottom-color", "#1c9041");
+    $(element).css("cursor", "initial");
+}
+
 // User interactions with Marky
 $(document).ready(function() {
-    $("body").load(function() {
-        $("body").fadeIn("400");
+    $("body").fadeIn("400");
+
+    // Make the selected <li> 'active'
+    $("li").click(function() {
+        var li_ID = $(this).attr("id");
+        resetThenColor("#" + li_ID);
     });
 });
