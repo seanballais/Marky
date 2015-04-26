49204563ac0d7ff89cfdcd917991015c64ba3d10
Sean Francis N. Ballais <sfballais123@gmail.com>
2015-04-26 19:05:44 +0800

    Added fading in body

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
 if (typeof jQuery === "undefined") { throw new Error("Marky requires jQuery") }
 
 // User interactions with Marky
+$(document).ready(function() {
+    $("body").load(function() {
+        $("body").fadeIn("400");
+    });
+});
